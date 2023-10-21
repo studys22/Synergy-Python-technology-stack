d3afc6c091c3a704621ad0bbd89488bcd2e26686
studys22 <studys22@mail.ru>
2023-10-21 16:17:20 +0300

    Correction of comments on the solution to the task for lesson #4
    Урок №4. float, int и арифметические операции

--- 04/4_1.py
+++ 04/4_1.py
@@ -2,4 +2,5 @@ a, b = map(
     float,
     input("Enter the length and width of the rectangle separated by a space: ").split(),
 )
-print("The area of the triangle is equal to", a * b)
+print("The area of the rectangle is equal to", a * b)
+print("The perimeter of the rectangle is equal to", 2 * (a + b))
